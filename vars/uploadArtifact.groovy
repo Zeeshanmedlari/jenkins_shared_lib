a31cea56f41c5bbdce2fd8ccabc16f886550358d
Zeeshanmedlari <97236604+Zeeshanmedlari@users.noreply.github.com>
2023-11-22 13:33:21 +0530

    Update uploadArtifact.groovy

--- vars/uploadArtifact.groovy
+++ vars/uploadArtifact.groovy
@@ -1,21 +1,24 @@
 stage('Deploy to Artifactory') {
-            steps {
-                script {
-                    def server = Artifactory.server 'Artifactory'
-                    def buildInfo = Artifactory.newBuildInfo()
+    steps {
+        script {
+            def server = Artifactory.server 'Artifactory'
+            def buildInfo = Artifactory.newBuildInfo()
 
-                    // Deploy artifacts to Artifactory
-                    server.upload spec: {
-                        "files": [
-                            {
-                                "pattern": "target/*.jar",
-                                "target": "java-application-libs-snapshot-local/${BUILD_NUMBER}/",
-                                "props": "build.name=${JOB_NAME};build.number=${BUILD_NUMBER}"
-                            }
-                        ]
-                    }, buildInfo: buildInfo
+            // Deploy artifacts to Artifactory
+            server.upload(
+                spec: """{
+                    "files": [
+                        {
+                            "pattern": "target/*.jar",
+                            "target": "java-application-libs-snapshot-local/${BUILD_NUMBER}/",
+                            "props": "build.name=${JOB_NAME};build.number=${BUILD_NUMBER}"
+                        }
+                    ]
+                }""",
+                buildInfo: buildInfo
+            )
 
-                    server.publishBuildInfo buildInfo
-                }
-            }
+            server.publishBuildInfo buildInfo
         }
+    }
+}
